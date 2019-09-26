80ffe376c495c2472ae690cd25e71f9612e96564
alabliuk <andrelabliuk@gmail.com>
2019-09-26 02:41:11 -0300

    python

--- Python/Curso em Vídeo [YouTube]/desafios/Curso Python #07 - Operadores Aritméticos.py
+++ Python/Curso em Vídeo [YouTube]/desafios/Curso Python #07 - Operadores Aritméticos.py
@@ -1,3 +1,37 @@
-# https://www.youtube.com/watch?v=Vw6gLypRKmY&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t=0s
+# https://www.youtube.com/watch?v=Vw6gLypRKmY&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t
 
 # Desafio 05
+# x = int(input('Digite um numero --> '))
+# print(f'{x-1} <-- {x} --> {x+1}')
+
+# Desafio 06
+# x = int(input('Digite um numero --> '))
+# print(f'Numero: {x}, Dobro: {x*2}, Triplo: {x*3}, Raiz Quadrada {x**(1/2)}')
+
+# Desafio 07
+# n1 = int(input('Digite a nota 1: '))
+# n2 = int(input('Digite a nota 2: '))
+# print(f'Média: {(n1+n2)/2}')
+
+# Desafio 08
+# metros = int(input('metros: '))
+# print(f'centimetros: {metros * 100}')
+# print(f'milimetros: {metros * 1000}')
+
+# Desafio 09
+# tab = int(input('Exibir a tabuada do numero: '))
+# aux = int(0)
+# while aux <= 10:
+#     print(f'{tab} x {aux} = {tab*aux}')
+#     aux += 1
+
+# Desafio 10
+real = int(input('Real: '))
+valorDolar = float(3.27)
+print(f'Conversão: BRL {real} --> USD {real * valorDolar} || Cotação Dolar: {valorDolar}')
+
+# Desafio 11
+
+# Desafio 12
+
+# Desafio 13
