97e7161ae1115d01cc5da19cf3b8b318d15e63be
AndreL1 <andre.leme@verifone.com>
2019-10-23 19:27:45 -0300

    exercicio python 10 - em andamento

---
+++ Python/Curso em Vídeo [YouTube]/desafios/Curso Python #10 - Condições (Parte 1).py
@@ -0,0 +1,84 @@
+# https://www.youtube.com/watch?v=K10u3XIf1-Q&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6
+
+from random import randint
+
+# # Desafio 28
+# print('\n\nDesafio 28')
+# numUser = int(
+#     input('Tente adivinhar o numero escolhido pelo computador (Range: 0 a 5): '))
+# numPc = randint(0, 5)
+# print(f'Chute: {numUser} \nNumero Gerado: {numPc}')
+# if(numPc == numUser):
+#     print('Você Ganhou!')
+# else:
+#     print('Você Perdeu!')
+
+
+# # Desafio 29
+# print('\n\nDesafio 29')
+# print('Limite de velocidade: 80km/h')
+# km = int(input('Velocidade do automovel: '))
+# if(km > 80):
+#     print(f'Multa: {(km - 80)*7}')
+# else:
+#     print('Veiculo não multado')
+
+
+# # Desafio 30
+# print('\n\nDesafio 30')
+# n = int(input('NÚMERO --> '))
+# if(n % 2):
+#     print('IMPAR')
+# else:
+#     print('PAR')
+
+
+# # Desafio 31
+# print('\n\nDesafio 31')
+# km = int(input('Distancia em KM da viagem: '))
+# if(km > 200):
+#     print(f'Valor por KM: R$0,45 \nValor Total: {km*0.45}')
+# else:
+#     print(f'Valor por KM: R$0,50 \nValor Total: {km*0.50}')
+
+
+# # Desafio 32
+# print('\n\nDesafio 32')
+# ano = int(input('Ano: '))
+# if(ano % 4):
+#     print('Ano NÃO Bissexto')
+# else:
+#     print('Ano Bissexto')
+
+
+# # Desafio 33
+# print('\n\nDesafio 33')
+# print('Digite 3 numeros: ')
+# n1 = int(input('Digite o numero 1: '))
+# n2 = int(input('Digite o numero 2: '))
+# n3 = int(input('Digite o numero 3: '))
+
+# if(n1 > n2):
+#     if(n1 > n3):
+#         print(f'Maior numero: {n1}')
+#         if(n2 > n3):
+#             print(f'Menor numero: {n3}')
+#         else:
+#             print(f'Menor numero: {n2}')
+#     else:
+#         print(f'Maior numero: {n3}')
+#         print(f'Menor numero: {n2}')
+# elif(n2 > n3):
+#     print(f'Maior numero: {n2}')
+#     print(f'Menor numero: {n1}')
+# else:
+#     print(f'Maior numero: {n3}')
+#     print(f'Menor numero: {n1}')
+
+
+# Desafio 34
+print('\n\nDesafio 34')
+
+
+# Desafio 35
+print('\n\nDesafio 35')
