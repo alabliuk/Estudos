33e34577e948725e4a92465f2dd4a2f97b7ccfee
AndreL1 <andre.leme@verifone.com>
2019-10-24 18:56:43 -0300

    exercicio python 12 - em andamento

---
+++ Python/Curso em Vídeo [YouTube]/desafios/Curso Python #012 - Condições Aninhadas.py
@@ -0,0 +1,44 @@
+# https://www.youtube.com/watch?v=j9bYDjaAYzw&list=PLHz_AreHm4dk_nZHmxxf_J0WRAqy5Czye&t=0s
+
+# # Desafio 36
+# print('\n\nDesafio 36')
+# valor = float(input('Valor do imovel: '))
+# salario = float(input('Salario: '))
+# parcelas = int(input('Parcelas: '))
+
+# parcela_limite = salario * 0.30
+
+# if(parcela_limite > valor/parcelas):
+#     print('Financiamento Aprovado!')
+# else:
+#     print('Financiamento REPROVADO!')
+
+
+# # Desafio 37
+# print('\n\nDesafio 37')
+# valor = int(input('Digite um numero: '))
+# menu = int(input(f'Converter o numero {valor} para: \n1 - Binario\n2 - Octal\n3 - Hexadecimal\n\nConversão escolhida --> '))
+
+# if(menu == 1):
+#     print(f'Conversão Binaria: {valor} --> {valor:b}')
+# elif(menu == 2):
+#     print(f'Conversão Octal: {valor} --> {valor:o}')
+# elif(menu == 3):
+#     print(f'Conversão Hexadecimal: {valor} --> {valor:x}')
+
+
+# # Desafio 38
+# print('\n\nDesafio 38')
+# n1 = int(input('Digite n1: '))
+# n2 = int(input('Digite n2: '))
+
+# if(n1 > n2):
+#     print(f'{n1} (n1) é MAIOR que {n2} (n2)')
+# elif(n1 == n2):
+#     print(f'{n1} (n1) é IGUAL a {n2} (n2)')
+# else:
+#     print(f'{n1} (n1) é MENOR que {n2} (n2)')
+
+
+# Desafio 39
+print('\n\nDesafio 39')
