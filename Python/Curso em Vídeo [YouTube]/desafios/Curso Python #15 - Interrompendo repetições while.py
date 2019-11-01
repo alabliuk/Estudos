e9fac8599214001099fd3206add6642fdc24b549
AndreL1 <andre.leme@verifone.com>
2019-11-01 19:19:42 -0300

    Execicio python 15 - em andamento

---
+++ Python/Curso em Vídeo [YouTube]/desafios/Curso Python #15 - Interrompendo repetições while.py
@@ -0,0 +1,30 @@
+# https://www.youtube.com/watch?v=1OFp_-R2B2A&list=PLHz_AreHm4dk_nZHmxxf_J0WRAqy5Czye
+
+# # Desafio 66
+# print('\n\nDesafio 66')
+# soma = 0
+# c = 0
+# while(True):
+#     valor = int(input('Digite um valor (999 para sair) --> '))
+#     soma += valor
+#     if(valor == 999):
+#         print(f'A soma dos {c} valores foi de {soma -999}!')
+#         break
+#     c += 1
+
+
+# # Desafio 67
+# print('\n\nDesafio 67')
+# while(True):
+#     print('-'*20)
+#     tabuada = int(input('Tabuada a ser renderizada --> '))
+#     if(tabuada > 0):
+#         for c in range(1, 11):
+#             print(f'{tabuada} x {c:2} = {tabuada*c:3}')
+#     else:
+#         break
+
+
+# Desafio 68
+print('\n\nDesafio 68')
+print('-= JOGO do PAR ou IMPAR =-')
